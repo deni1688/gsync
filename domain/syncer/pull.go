0aa48bf5833a28104acb02c07a79cf997fdbf256
King Denis <denis.junuzovic@fleetster.net>
2022-04-29 16:41:11 +0200

    tries routines again

--- domain/syncer/pull.go
+++ domain/syncer/pull.go
@@ -3,6 +3,7 @@ package syncer
 import (
 	"log"
 	"os"
+	"sync"
 )
 
 func (g syncService) Pull(sf SyncFile) error {
@@ -54,31 +55,43 @@ func (g syncService) removeFilesFromLocal(sf SyncFile, files []SyncFile) error {
 }
 
 func (g syncService) downloadFiles(sf SyncFile, files []SyncFile) error {
+	var wg sync.WaitGroup
+	errCh := make(chan error)
+
 	for _, file := range files {
 		fullPath := GetFullPath(sf.Path, file.Name)
 		log.Printf("Pulling %s", fullPath)
 
-		if g.drive.IsDir(file) {
-			if err := CreateDir(fullPath); err != nil {
-				return err
-			}
+		wg.Add(1)
+		go func(file SyncFile) {
+			if g.drive.IsDir(file) {
+				if err := CreateDir(fullPath); err != nil {
+					errCh <- err
+				}
 
-			file.Path = fullPath
+				file.Path = fullPath
 
-			if err := g.Pull(file); err != nil {
-				return err
-			}
-		} else {
-			data, err := g.drive.GetFile(file)
-			if err != nil {
-				return err
-			}
+				if err := g.Pull(file); err != nil {
+					errCh <- err
+				}
+			} else {
+				data, err := g.drive.GetFile(file)
+				if err != nil {
+					errCh <- err
+				}
 
-			if err = os.WriteFile(fullPath, data, 0700); err != nil {
-				return err
+				if err = os.WriteFile(fullPath, data, 0700); err != nil {
+					errCh <- err
+				}
 			}
-		}
+			wg.Done()
+		}(file)
+	}
+	wg.Wait()
+
+	if len(errCh) < 1 {
+		errCh <- nil
 	}
 
-	return nil
+	return <-errCh
 }
